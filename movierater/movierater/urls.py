0ca93f8157bf33e86d6fa9a8abe5adddb19f9362
adixoncode <adrian.trzebuniak@gmail.com>
2018-08-24 21:09:01 +0200

    change login,logout urls

--- movierater/movierater/urls.py
+++ movierater/movierater/urls.py
@@ -10,5 +10,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login, name='login'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
